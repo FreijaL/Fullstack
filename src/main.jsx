cefff12a317a535cbcbb777b4326099d1e7959c7
sebbanwalker <seb10walker@hotmail.com>
2023-11-09 00:24:10 +0100

    header progress, fixed shell

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
+import Test from './Test.jsx'
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
