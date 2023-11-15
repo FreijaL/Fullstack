bf04bed6f24e6fc8a8ef34ed03fb96641be5906e
FreijaL <110614499+FreijaL@users.noreply.github.com>
2023-11-15 11:06:27 +0100

    Menu finished and module fix on header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import './Header.scss';
+import style from './Header.module.scss';
 import Pizza from '/svg/pizza.svg';
 import Cart from '/svg/cart.svg';
 
 const Header = () => {
     return (
-        <div className="header-container">
-            <img className="header-pizza" src={Pizza}></img>
-            <h1 className="header-title">Valhalla</h1>
-            <img className="header-cart" src={Cart}></img>
+        <div className={style.headerContainer}>
+            <img className={style.headerPizza} src={Pizza}></img>
+            <h1 className={style.headerTitle}>Valhalla</h1>
+            <img className={style.headerCart} src={Cart}></img>
         </div>
     )
 }
