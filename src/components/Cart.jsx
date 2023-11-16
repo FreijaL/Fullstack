b55300dba4e9cb127e8fea9e3e2ac551a129010f
Mercenaress <linden9707@gmail.com>
2023-11-16 11:31:29 +0100

    Cart start

---
+++ src/components/Cart.jsx
@@ -0,0 +1,14 @@
+import style from './Cart.module.scss';
+
+function Cart() {
+    return ( 
+        <article>
+            <h3>Orderöversikt</h3>
+            <section>
+                
+            </section>
+        </article>
+     );
+}
+
+export default Cart;
